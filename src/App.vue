29a9a779a5e42b895e0337a46f11cf003b3c09fc
tpetersn <tpetersen1525@gmail.com>
2025-06-08 15:05:30 +0200

    Made some changes to the layout

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,42 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import {
+  BImg,
+  BNavbar,
+  BNavbarBrand,
+  BNavbarNav,
+  BNavbarToggle,
+  BNavItem,
+  BOffcanvas
+} from "bootstrap-vue-next";
 </script>
 
 <template>
-  <!-- #region template -->
   <BNavbar v-b-color-mode="'dark'" :toggleable="true" variant="primary">
-    <BNavbarBrand href="#">NavBar</BNavbarBrand>
+    <BNavbarBrand href="#">Menu</BNavbarBrand>
     <BNavbarToggle target="nav-offcanvas" />
-    <BOffcanvas id="nav-offcanvas" title="Offcanvas" placement="end" is-nav>
+    <BOffcanvas id="nav-offcanvas" title="Menu" placement="end" is-nav>
       <BNavbarNav>
-        <BNavItem href="#">Link</BNavItem>
-        <BNavItem href="#" disabled>Disabled</BNavItem>
-      </BNavbarNav>
-      <!-- Right aligned nav items -->
-      <BNavbarNav class="ms-auto mb-2 mb-lg-0">
-        <BNavItemDropdown text="Lang" right>
-          <BDropdownItem href="#">EN</BDropdownItem>
-          <BDropdownItem href="#">ES</BDropdownItem>
-          <BDropdownItem href="#">RU</BDropdownItem>
-          <BDropdownItem href="#">FA</BDropdownItem>
-        </BNavItemDropdown>
-        <BNavItemDropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <BDropdownItem href="#">Profile</BDropdownItem>
-          <BDropdownItem href="#">Sign Out</BDropdownItem>
-        </BNavItemDropdown>
-      </BNavbarNav>
-      <BNavForm class="d-flex">
-        <BFormInput class="me-2" placeholder="Search" />
-        <BButton type="submit" variant="outline-success">Search</BButton>
-      </BNavForm>
-    </BOffcanvas>
-  </BNavbar>
-  <!-- #endregion template -->
-
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <BNavItem href="#">Service &amp; Pricing</BNavItem>
+        <BNavItem href="#">Book An Appointment</BNavItem>
+        <BNavItem href="#">About Us</BNavItem>
+        <BNavItem href="#">Contact Us</BNavItem>
+        <BNavItem href="#">Shop Hours</BNavItem>
+      </BNavbarNav>
+    </BOffcanvas>
+  </BNavbar>
 
+  <header>
+    <BImg src="https://picsum.photos/1280/400/?image=42" fluid alt="Responsive image" />
+  </header>
   <RouterView />
 </template>
+
+<style lang="css">
+
+</style>
 
 <style scoped>
 header {
@@ -60,60 +44,9 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
 
 @media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
 }
 </style>
